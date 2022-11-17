3533eec4e04faa9fac6233bffef881e23b2111f0
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-17 11:14:44 -0800

    save

--- src/NewSkillForm.js
+++ src/NewSkillForm.js
@@ -1,16 +1,18 @@
+import {useState} from 'react';
 
 function NewSkillList() {
+    
     return (
         <form className="NewSkillForm">
             <label>Skill </label>
             <input type="text" id='skill' name='skill'></input>
             <label>Level: </label>
             <select name='level' id='level'>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
             </select>
             <button type="submit">ADD SKILL</button>
         </form>
